b8e3ce918a50f6c66872efd0fc78014495e1e92c
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-04-22 06:06:55 +0600

    CRUD Operation added..server add to database

---
+++ __step.js
@@ -0,0 +1,25 @@
+/*
+* 1. create a node server with 5 steps
+* 1.1 create folder
+* 1.2 npm init
+* 1.3 npm i express cors mongodb
+* 1.4 script-dev: nodemon index.js
+* 1.5 use 5 steps to create a node server
+*
+*=====================
+*create atlas Account
+*=====================
+* 1.sign up.google access
+* 2.create cluster
+* 3.create user dbUser and password
+* 4.Network access  --> ip address: allow all
+* 5.database > connect > code copy
+*------------------------------------
+*CRUD Operation
+*==================================== 
+* 1.node mongodb CRUD > Fundamentals
+* 2. create async run function
+* 
+****
+
+*/
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -10,6 +11,29 @@ app.use(express.json())
 
 //user: dbuser1
 //password: zsbvUSyfOyLrZylL
+
+
+
+const uri = "mongodb+srv://dbuser1:zsbvUSyfOyLrZylL@cluster0.bnf0w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try{
+        await client.connect()
+        const userCollection = client.db('car-genius').collection('carUser')
+        const carUser = {
+            carName: 'lemborgini',
+            mode: '2022'
+        }
+        const result = await userCollection.insertOne(carUser);
+        console.log(`create a user and find userId ${result.insertedId}`)
+    }
+    finally{
+        await client.close()
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/',(req,res) => {
     console.log(req)
